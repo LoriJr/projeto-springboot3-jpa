a28e4fad27997c3fd8354d2bf849ef777863dd61
lorijr <jrsilva.lorivas@gmail.com>
2025-08-24 10:53:34 -0300

    payment, on-to-one association

--- src/main/java/com/louproject/course/repositories/OrderItemRepository.java
+++ src/main/java/com/louproject/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.louproject.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.louproject.course.entities.OrderItem;
+import com.louproject.course.entities.pk.*;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 }
